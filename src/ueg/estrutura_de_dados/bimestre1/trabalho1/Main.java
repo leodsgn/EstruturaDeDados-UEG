6467c89daec56353cfaea9d2c2e6c8f480f9a708
Leonardo Ribeiro @leodsgn <leoribeiro.dsgn@gmail.com>
2015-04-29 20:52:19 -0300

    First Commit!

---
+++ src/ueg/estrutura_de_dados/bimestre1/trabalho1/Main.java
@@ -0,0 +1,10 @@
+package ueg.estrutura_de_dados.bimestre1.trabalho1;
+
+/**
+ * Created by root on 4/29/2015.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, fucking world!");
+    }
+}
