02ae2a212787c716537d9b22d4504177ff016e0c
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2015-06-12 21:30:01 -0300

    Torre de Hanoi

---
+++ src/ueg/estrutura_de_dados/bimestre2/Trabalho1/Main.java
@@ -0,0 +1,23 @@
+package ueg.estrutura_de_dados.bimestre2.Trabalho1;
+
+import java.util.Scanner;
+
+/**
+ * Created by @leodsgn on 12/06/15.
+ */
+public class Main {
+    // método main para restar o programa!
+    public static void main(String[] args) {
+
+        int n; // número de discos
+
+        // recebe o nú	mero de discos digitado pelo usuário
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o número de discos: ");
+        n = entrada.nextInt();
+
+        // executa o algoritmo iterativo das Torres de Hanói
+        TorreDeHanoi.hanoi(n);
+
+    }
+}
